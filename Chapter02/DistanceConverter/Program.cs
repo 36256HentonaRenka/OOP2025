c9888c1efb7210b8480fa533c612e7c02253c38a
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-04-21 10:10:23 +0900

    コメント追加

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace DistanceConverter {
     internal class Program {
+        //コマンドライン引数で指定された範囲のフィートとメールの対応表を出力する
         static void Main(string[] args) {
 
             int start = int.Parse(args[1]);
