4cbe592f4eed82e58b93ebe4dc77f1292f0c59d6
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-04-23 11:01:40 +0900

    ヤードからメートルの変換表

--- Chapter02/演習問題/Excercise02/Program.cs
+++ Chapter02/演習問題/Excercise02/Program.cs
@@ -8,42 +8,31 @@ namespace Excercise02 {
             int end = int.Parse(args[2]);*/
 
             Console.WriteLine("***変換アプリ***");
-            Console.WriteLine("1.インチからメートル");
-            Console.WriteLine("2.メートルからインチ");
+            Console.WriteLine("1.ヤードからメートル");
+            Console.WriteLine("2.メートルからヤード");
             int atai = int.Parse(Console.ReadLine());
 
-            if (atai >= 3) {
-                Console.WriteLine("エラー");
+            if (atai == 1) {
+                Console.Write("変更前(ヤード)：");
+                int yard = int.Parse(Console.ReadLine());
+                PrintYardToMeter(yard);
 
-            } else if (atai == 1 || atai == 2) {
-                Console.Write("はじめ：");
-                int start = int.Parse(Console.ReadLine());
-
-                Console.Write("おわり：");
-                int end = int.Parse(Console.ReadLine());
-
-                if (atai == 1) {
-                    PrintInchToMeter(start, end);
-
-                } else if (atai == 2) {
-                    PrintMeterToInch(start, end);
-                }
+            } else if ( atai == 2) {
+                Console.Write("変更前(メーター)：");
+                int meter = int.Parse(Console.ReadLine());
+                PrintMeterToYard(meter);
             } 
             
         }
-        //インチからメートルへの変換表
-        static void PrintInchToMeter(int start, int end) {
-            for (int inch = start; inch <= end; inch++) {
-                double meter = inch * 0.0254;
-                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
-            }
+        //ヤードからメートルへの変換表
+        static void PrintYardToMeter(int yard) {
+            double meter = yard * 0.9144;
+            Console.WriteLine("変更後(メーター)：" + $"{meter:0.000}");
         }
-        //メートルからインチへの変換表
-        static void PrintMeterToInch(int start, int end) {
-            for (int meter= start; meter <= end; meter++) {
-                double inch = meter / 0.0254;
-                Console.WriteLine($"{meter}m = {inch:0.0000}inch");
-            }
+        //メートルからヤードへの変換表
+        static void PrintMeterToYard(int meter) {
+            double yard = meter / 0.9144;
+            Console.WriteLine("変更後(ヤード)" + $"{yard:0.000}");
         }
 
         
