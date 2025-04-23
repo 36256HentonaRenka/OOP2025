3c9e1068416b84df94d89eb5c9ea2d99fbb7a69f
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-04-23 09:48:38 +0900

    変換アプリ

--- Chapter02/演習問題/Excercise02/Program.cs
+++ Chapter02/演習問題/Excercise02/Program.cs
@@ -1,15 +1,35 @@
-﻿namespace Excercise02 {
+﻿using System.Runtime.CompilerServices;
+
+namespace Excercise02 {
     internal class Program {
         static void Main(string[] args) {
 
-            int start = int.Parse(args[1]);
-            int end = int.Parse(args[2]);
+            /*int start = int.Parse(args[1]);
+            int end = int.Parse(args[2]);*/
+
+            Console.WriteLine("***変換アプリ***");
+            Console.WriteLine("1.インチからメートル");
+            Console.WriteLine("2.メートルからインチ");
+            int atai = int.Parse(Console.ReadLine());
+
+            if (atai >= 3) {
+                Console.WriteLine("エラー");
+
+            } else if (atai == 1 || atai == 2) {
+                Console.Write("はじめ：");
+                int start = int.Parse(Console.ReadLine());
+
+                Console.Write("おわり：");
+                int end = int.Parse(Console.ReadLine());
+
+                if (atai == 1) {
+                    PrintInchToMeter(start, end);
+
+                } else if (atai == 2) {
+                    PrintMeterToInch(start, end);
+                }
+            } 
             
-            //範囲
-            if (args.Length >= 1 && args[0] == "-tom") {
-                PrintInchToMeter(start, end);
-            
-            }
         }
         //インチからメートルへの変換表
         static void PrintInchToMeter(int start, int end) {
@@ -18,6 +38,14 @@
                 Console.WriteLine($"{inch}inch = {meter:0.0000}m");
             }
         }
+        //メートルからインチへの変換表
+        static void PrintMeterToInch(int start, int end) {
+            for (int meter= start; meter <= end; meter++) {
+                double inch = meter / 0.0254;
+                Console.WriteLine($"{meter}m = {inch:0.0000}inch");
+            }
+        }
+
         
     }
 }
