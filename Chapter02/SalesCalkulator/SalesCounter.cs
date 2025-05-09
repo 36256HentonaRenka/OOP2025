af31617ed51ca1639ced2866f801670d3f492445
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-09 14:56:56 +0900

    メソッドの移動

--- Chapter02/SalesCalkulator/SalesCounter.cs
+++ Chapter02/SalesCalkulator/SalesCounter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SalesCalkulator{
     //売り上げ集計クラス
-    public class SalesCounter{
+    public class SalesCounter {
         private readonly List<Sale> _sales;
 
         public SalesCounter(List<Sale> sales) {
@@ -24,6 +24,23 @@ namespace SalesCalkulator{
             }
             return dict;
         }
+        //売り上げデータを読みこみSaleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            //売り上げデータを入れるリストオブジェクトを生成
+            List<Sale> sales = new List<Sale>();
+            //ファイルを一気に読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amout = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
     }
-
 }
