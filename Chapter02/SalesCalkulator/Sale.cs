0512f050f62d40e6dbaf399601a62bff9f787c50
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-09 13:55:09 +0900

    売り上げ集計プログラムの初版完成

---
+++ Chapter02/SalesCalkulator/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalkulator {
+    //売り上げクラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; } = String.Empty;
+        //商品カテゴリー
+        public string ProductCategory { get; set; } = String.Empty;
+        //売上高
+        public int Amout { get; set; }
+    }
+}
---
+++ Chapter02/SalesCalkulator/SalesCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalkulator{
+    //売り上げ集計クラス
+    public class SalesCounter{
+        private readonly List<Sale> _sales;
+
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        //店舗別売り上げを求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amout;
+                else
+                    dict[sale.ShopName] = sale.Amout;
+            }
+            return dict;
+        }
+    }
+
+}
