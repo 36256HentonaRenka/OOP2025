a768baae5299c88169526220dd9d5c425c122bec
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-13 10:35:59 +0900

    2-1-2

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01{
+namespace Exercise01 {
     //2-1-1
-    public class Song{
+    public class Song {
         //歌のタイトル
         public string Title { get; set; } = string.Empty;
         //アーティスト名
         public string ArtistName { get; set; } = string.Empty;
         //演奏時間
-        public int Length;
+        public int Length { get; set; }
+
+        //2-1-2
+        public Song(string Title,string ArtistName,int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+
     }
-    //2-1-2
-    
-    
 }
