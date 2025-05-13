6e19ab26e77ecb6a046e4a39b2ba53d5f8703486
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-13 10:10:31 +0900

    2-1-1

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //2-1-1
+    public class Song{
+        //歌のタイトル
+        public string Title { get; set; } = string.Empty;
+        //アーティスト名
+        public string ArtistName { get; set; } = string.Empty;
+        //演奏時間
+        public int Length;
+    }
+    //2-1-2
+    
+    
+}
