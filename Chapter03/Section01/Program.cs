59d1b1a5e0a45e7abc133d96f458283b019aa6bc
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-16 14:56:56 +0900

    P71まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,27 +1,27 @@
 ﻿namespace Section01 {
     internal class Program {
 
-        //public delegate bool Judgement(int value);//デリゲートの宣言
-
         static void Main(string[] args) {
+           var cities = new List<string> {
+            "Tokyo",
+            "New Delhi",
+            "Bangkok",
+            "London",
+            "Paris",
+            "Berlin",
+            "Canberra",
+            "Hong Kong",
+           };
 
-            /*Console.WriteLine("カウントしたい数値:");
-            int num = int.Parse(Console.ReadLine());*/
+            var lowerList = cities.ConvertAll(s => s.ToLower());
+            lowerList.ForEach(s => Console.WriteLine(s));
 
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            
-            Console.WriteLine(Count(numbers, n=> 5 <= n && n < 10));
-        }
- 
-        static int Count(int[] numbers,Predicate<int> judge) { 
-            var count = 0;
-            foreach (var n in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if ( judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+            Console.WriteLine("");
+
+            var highList = cities.ConvertAll(s => s.ToUpper());
+            highList.ForEach(s => Console.WriteLine(s));
+           
+
         }
     }
 }
