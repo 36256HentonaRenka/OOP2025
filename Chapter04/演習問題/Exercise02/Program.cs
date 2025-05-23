dc04b5ed97a229368abdc868f4d527785bdd9542
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-23 15:24:11 +0900

    4-2-1完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,7 +1,41 @@
-﻿namespace Exercise02 {
+﻿using System;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        
+        }
+
+        private static void Exercise1() {
+            var line = Console.ReadLine();
+            var num = int.Parse(line);
+
+            if (num < 0) {
+                Console.WriteLine(num);
+
+            } else if (0 <= num && num <100 ) {
+                Console.WriteLine(num * 2);
+
+            } else if (100 <= num && num < 500) {
+                Console.WriteLine(num * 3);
+
+            }else if (500 <= num) {
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Exercise2() {
+            
+        }
+
+        private static void Exercise3() {
+            
         }
     }
 }
