5fc6b82f175491b19633bf3c1d159c13fbb2632a
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-05-27 09:48:28 +0900

    7-1-1完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -13,7 +13,7 @@ namespace Exercise02 {
         }
 
         private static void Exercise1() {
-            var line = Console.ReadLine();
+            /*var line = Console.ReadLine();
             var num = int.Parse(line);
 
             if (num < 0) {
@@ -27,15 +27,59 @@ namespace Exercise02 {
 
             }else if (500 <= num) {
                 Console.WriteLine(num);
+            }*/
+            var line = Console.ReadLine();
+            if(int.TryParse(line,out var num)) {
+                if (num < 0 ) {
+                    Console.WriteLine(num);
+
+                } else if ( num < 100) {
+                    Console.WriteLine(num * 2);
+
+                } else if ( num < 500) {
+                    Console.WriteLine(num * 3);
+
+                } else  {
+                    Console.WriteLine(num);
+                }
             }
         }
 
         private static void Exercise2() {
-            
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                switch (num) {
+                    case < 0:
+                    Console.WriteLine(num);
+                        break;
+
+                    case < 100:
+                    Console.WriteLine(num * 2);
+                        break;
+
+                    case < 500:
+                    Console.WriteLine(num * 3);
+                        break;
+
+                    default:
+                    Console.WriteLine(num);
+                        break;
+                }
+            }
         }
 
         private static void Exercise3() {
-            
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var num)) {
+                var text = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                    _  => num
+                };
+                Console.WriteLine(text);
+
+            }
         }
     }
 }
---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Where(s=> s%8==0).Sum());
+
+            /*foreach(var num in numbers) {
+                Console.WriteLine(num);
+
+            }*/
+
+
+
+        }
+    }
+}
