47f8c025d36a00f72747948d7483a07a28a3e2e4
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-06-06 15:10:21 +0900

    5-1-1完成

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var appVer1 = new AppVersion(5,1);
+            var appVer2 = new AppVersion(5,1);
+
+            if (appVer1 == appVer2) {
+                Console.WriteLine("等しい");
+            }else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+
+    public record AppVersion(int m ,int mi, int b = 0, int r =  0) {
+        public int Major { get; init; } = m;
+        public int Minor { get; init; } = mi;
+        public int Build { get; init; } = b;
+        public int Revision { get; init; } = r;
+
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //5.1.1
+    public　class YearMonth{
+
+        int Year { get; }
+        int Month{ get; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //5.1.2
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+
+        //5.1.3
+        //public YearMonth AddOneMonth() { 
+        
+        //}
+            
+
+        //5.1.4
+        public override string ToString() {
+            
+
+            return base.ToString();
+        }
+
+    }
+}
