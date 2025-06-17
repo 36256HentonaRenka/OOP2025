5b1467950ee565544a03f8b45ed7acf68a24f180
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-06-17 11:09:51 +0900

    設問１＿Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+
+        public string Name { get; private set; } = String.Empty;
+
+        public string Subject { get; private set; }=String.Empty;
+
+        public int Score { get; private set; }
+    }
+}
