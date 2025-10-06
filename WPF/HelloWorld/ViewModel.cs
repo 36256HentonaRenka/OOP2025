052760c1ba5d6d9f6e716829e71ab7e203415a59
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-10-06 15:59:44 +0900

    PrismObserver

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -10,24 +10,15 @@ namespace HelloWorld{
 
         public ViewModel() {
             ChangeMessageCommand = new DelegateCommand<string>(
-                (par)=> GreetingMessage = par 
-            );
+                (par) => GreetingMessage = par,
+                (par) => GreetingMessage != par)
+                .ObservesProperty(() => GreetingMessage);
         }
 
         private string _greetingMessage = "Hello World!";
         public string GreetingMessage { 
             get => _greetingMessage;
-            set  {
-                if (SetProperty(ref _greetingMessage, value)) {
-                    CanChangeMessage = false;
-                }
-            }
-        }
-
-        private bool _canChangeMessage = true;
-        public bool CanChangeMessage {
-            get => _canChangeMessage;
-            private set => SetProperty(ref _canChangeMessage, value);
+            set => SetProperty(ref _greetingMessage, value);
         }
 
         public string NewMessage1 { get; } = "lucky";
---
+++ WPF/PrismObservesSample/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismObservesSample{
+    class MainWindowViewModel : BindableBase{
+        private string _input1 = "";
+        public string Input1 {
+            get => _input1;
+            set => SetProperty(ref _input1,value);
+        }
+
+        private string _input2 = "";
+        public string Input2 {
+            get => _input2;
+            set => SetProperty(ref _input2,value);
+        }
+
+        private string _result = "";
+        public string Result {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        //コンストラクタ
+        public MainWindowViewModel() {
+            SumCommand = new DelegateCommand(ExcuteSum, canExecuteSum)
+            .ObservesProperty(() => Input1)
+            .ObservesProperty(() => Input2);
+        }
+
+        public DelegateCommand SumCommand { get; }
+
+        //足し算の処理
+        private void ExcuteSum() {
+            var n1 = double.Parse(Input1);
+            var n2 = double.Parse(Input2);
+
+            Result = (n1 + n2).ToString();
+
+        }
+        //足し算処理を実行できるか否かチェック
+        private bool canExecuteSum() {
+            return (int.TryParse(Input1, out _) && (int.TryParse(Input2, out _))) ;
+        }
+    }
+}
