1f61d008cc6f60c33fc3cb4f8d89354d8bf861f1
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-10-16 15:22:08 +0900

    更新まで終了

--- SQLite/Sample/MainWindow.xaml.cs
+++ SQLite/Sample/MainWindow.xaml.cs
@@ -20,19 +20,19 @@ namespace Sample;
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window{
-    private ObservableCollection<Person> _persons = new ObservableCollection<Person>();
+    private List<Person> _persons = new List<Person>();
 
     public MainWindow(){
         InitializeComponent();
-        //ReadDatabase();
-        _persons.Add(new Person { Id = 1, Name = "aaaaa", Phone = "12345678901" });
+        ReadDatabase();
+
         PersonListView.ItemsSource = _persons;
     }
 
     private void ReadDatabase() {
         using (var connection = new SQLiteConnection(App.databasePath)) {
             connection.CreateTable<Person>();
-            //_persons = connection.Table<Person>().ToList();
+            _persons = connection.Table<Person>().ToList();
         }
     }
 
@@ -51,9 +51,58 @@ public partial class MainWindow : Window{
 
     private void ReadButton_Click(object sender, RoutedEventArgs e) {
 
-        _persons.Add(new Person { Id = 1, Name = "aaaaa", Phone = "12345678901" });
+        ReadDatabase();
+        PersonListView.ItemsSource = _persons;
+        
+    }
 
-        //ReadDatabase();
+    private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+        var item = PersonListView.SelectedItem as Person;
+        if(item == null) {
+            MessageBox.Show("行を選択してください");
+            return;
+        }
 
+        //データベース接続
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Person>();
+            connection.Delete(item);  //データから選択された項目を削除
+            ReadDatabase();
+            PersonListView.ItemsSource = _persons;
+        }
+
+    }
+
+    private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+        var filterList = _persons.Where(s=> s.Name.Contains(SearchTextBox.Text));
+        
+        PersonListView.ItemsSource = filterList;
+    }
+
+    //リストビューからレコード選択
+    private void PersonListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        var item = PersonListView.SelectedItem as Person;
+        
+        NameText.Text = item?.Name;
+        PhoneText.Text = item?.Phone;
+
+    }
+
+    private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+        var selectedPerson = PersonListView.SelectedItem as Person;
+        if (selectedPerson == null) return;
+
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Person>();
+
+            var person = new Person() {
+                Id = selectedPerson.Id,
+                Name = NameText.Text,
+                Phone = PhoneText.Text
+            };
+            connection.Update(person);
+            ReadDatabase();
+            PersonListView.ItemsSource = _persons;
+        }
     }
 }
