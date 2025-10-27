55be4093923bf116a2178c3b0dbc2991c3501292
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-10-27 15:28:17 +0900

    Exercise1_2完成

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books.Where(b => b.CategoryId == 1)
+                                     .Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();
+
+            var book = Library.Books.Where(b => b.PublishedYear >= 2021)
+                                    .MinBy(b=> b.Price);
+            Console.WriteLine(book);
+
+            Console.WriteLine();
+
+            var average = Library.Books.Average(b => b.Price);
+            var aboves = Library.Books
+                .Where(b => b.Price > average);
+            foreach(var books in aboves) {
+                Console.WriteLine(books);
+            }
+        }
+    }
+}
