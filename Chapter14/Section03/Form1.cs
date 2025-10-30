4a47476eaa6f3d89081d1cadb538aa052d925333
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-10-30 14:59:28 +0900

    コード4.7

---
+++ Chapter14/Section03/Form1.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+
+namespace Section03 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            var elapsed = await Task.Run(() => DoLongTimeWork());
+            toolStripStatusLabel1.Text = $"{elapsed}ミリ秒";
+        }
+        //戻り値のある同期メソッド
+        private long DoLongTimeWork() {
+            var sw = Stopwatch.StartNew();
+
+            System.Threading.Thread.Sleep(5000);
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
