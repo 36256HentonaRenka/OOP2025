c9aa9da0a35867be4f0f82537da447c89dfaa658
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-11-05 11:17:12 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            var hc = new HttpClient();
+            await GetHtmlExample(hc);
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://sports.yahoo.co.jp/basket/nba/";
+            var text = await httpClient.GetStreamAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
