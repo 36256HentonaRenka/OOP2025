f533026f585354e31fb1a011abeab03f080d3a4d
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-11-06 15:16:58 +0900

    並列処理ありとなしの比較サンプル

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Parallel.For(0,5,i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理{i}終了");
+            });
+
+            Console.WriteLine("並列処理なし");
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理{i}終了");
+            }
+        }
+    }
+}
--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace WebBrowser;
 
@@ -19,6 +22,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        InitializeAsync();
+    }
+
+    private async void InitializeAsync() {
+        await WebView.EnsureCoreWebView2Async();
+
+        WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationStartinged;
+    }
+
+    private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LoadingBar.Visibility = Visibility.Visible;
+        LoadingBar.IsIndeterminate = true;
+    }
+
+    private void CoreWebView2_NavigationStartinged(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
+        LoadingBar.Visibility = Visibility.Collapsed;
+        LoadingBar.IsIndeterminate = false;
     }
 
     private void BackButton_Click(object sender, RoutedEventArgs e) {
@@ -33,8 +54,12 @@ public partial class MainWindow : Window
         }
     }
 
-    private void GoButton_Click(object sender, RoutedEventArgs e) {
-        WebView.Source = new Uri(AddressBar.Text);
+    private void  GoButton_Click(object sender, RoutedEventArgs e) {
+        var url =AddressBar.Text.Trim();
+
+        if (string.IsNullOrWhiteSpace(url)) return;
+
+        WebView.Source = new Uri(url);
     }
 
     private void AddressBar_TextChanged(object sender, TextChangedEventArgs e) {
