9310689732d4c569f7bbf8bb33afd76b8622ab9e
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-11-06 13:47:40 +0900

    WebBrowserかんせい

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e) {
+        if (WebView.CanGoBack) {
+            WebView.GoBack();
+        }
+    }
+
+    private void FowardButton_Click(object sender, RoutedEventArgs e) {
+        if (WebView.CanGoForward) {
+            WebView.GoForward();
+        }
+    }
+
+    private void GoButton_Click(object sender, RoutedEventArgs e) {
+        WebView.Source = new Uri(AddressBar.Text);
+    }
+
+    private void AddressBar_TextChanged(object sender, TextChangedEventArgs e) {
+        
+    }
+}
