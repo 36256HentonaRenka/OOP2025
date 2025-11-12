ae53676beefab69564826554416bd2996a1694f0
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-11-12 09:19:35 +0900

    Exercise01.10.1完成

---
+++ Chapter10/Section01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = "./Greeting.txt";
+            if (File.Exists(filePath)) {
+                using var reader = new StreamReader(filePath,Encoding.UTF8);
+                while(!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var filePath = "";
+
+            var count = CountClass(filePath);
+
+        }
+        //Exercise01.10.1
+        public static int CountClass(string filePath) {
+            int count = 0;
+
+            using var reader = new StreamReader(filePath, Encoding.UTF8);
+            while (!reader.EndOfStream) {
+                var line = reader.ReadLine();
+                if (line.Contains("class")) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
