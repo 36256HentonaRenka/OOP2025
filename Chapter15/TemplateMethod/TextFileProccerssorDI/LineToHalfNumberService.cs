261861e884d79fc6e60673e8452a5c79eaf24d56
36256HentonaRenka <oibc36256@ojs.ac.jp>
2025-11-19 10:28:28 +0900

    15.1完成

--- Chapter15/TemplateMethod/TextFileProccerssorDI/LineToHalfNumberService.cs
+++ Chapter15/TemplateMethod/TextFileProccerssorDI/LineToHalfNumberService.cs
@@ -1,19 +1,36 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextFileProccerssorDI {
-    internal class LineToHalfNumberService : ITextFileService {
-        private int _count;
+    public class LineToHalfNumberService : ITextFileService {
+        //P３６２　問題15.1
+
+        private static Dictionary<char, char> _dictonary =
+            new Dictionary<char, char>() {
+                {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},{'６','6'},{'７','7'},{'８','8'},{'９','9'}
+            };   
+
+
+
         public void Initialize(string fname) {
-            _count = 0;
+            
         }
 
         public void Execute(string line) {
-            _count++;
-            Console.WriteLine(line.ToUpper());
+            /*string result = new string (
+                line.Select(c=> ('０' <= c && c <= '９') ? (char)(c - '０' + '０') : c).ToArray()
+            );
+            Console.WriteLine(result);*/
+
+            var s = Regex.Replace(line, "{０-９}", c => _dictonary[c.Value[0]].ToString());
+            Console.WriteLine(s);
+
+
         }
 
         public void Terminate() {
--- Chapter15/TemplateMethod/TextFileProccerssorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProccerssorDI/Program.cs
@@ -3,9 +3,12 @@
         static void Main(string[] args) {
             //var service = new LineCounterService();
             //var processor = new TextFileProccessor(service);
-            var service = new LineOutputService();
+            //var service = new LineOutputService();
+            //var processor = new TextFileProccessor(service);
+            var service = new LineToHalfNumberService();
             var processor = new TextFileProccessor(service);
-            Console.Write("パスの入力");
+            
+            Console.Write("パスの入力:");
             processor.Run(Console.ReadLine());
         }
     }
